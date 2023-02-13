7b601689faa8eb9dc10fbc3199059cea8b283eab
peter_califano <petercalifano.gs@gmail.com>
2023-02-13 15:01:53 +0100

    Revert "Revert "Added function "greyest_wrapper"""
    This reverts commit bf43806b6fa99ee24653c7bb44c7a7c1c49eadf7.

---
+++ simulator_antx_identified-single_axis/greyest_wrapper.m
@@ -0,0 +1,57 @@
+function [fitmodel, est_params, est_unc] = greyest_wrapper(data_to_fit, model_fun, theta0)
+%% PROTOTYPE
+% -------------------------------------------------------------------------------------------------------------
+%% DESCRIPTION
+% What the function does
+% -------------------------------------------------------------------------------------------------------------
+%% INPUT
+% in1 [dim] description
+% -------------------------------------------------------------------------------------------------------------
+%% OUTPUT
+% out1 [dim] description
+% -------------------------------------------------------------------------------------------------------------
+%% CHANGELOG
+% Date, User, brief summary of the modification
+% -------------------------------------------------------------------------------------------------------------
+%% DEPENDENCIES
+% -------------------------------------------------------------------------------------------------------------
+%% Future upgrades
+
+
+%% Function code
+% Guess parameters vector
+% theta0 = th_true.*[1.2 1.1 0.8 1.1 0.7 1.1];
+
+% Generate initial guess for greyest function
+% odefun = 'LongDyn_ODE';
+% Derivative of udot wrt u
+Xu = theta0(1);
+% Derivative of udot wrt q
+Xq = theta0(2);
+% Derivative of qdot wrt u
+Mu = theta0(3);
+% Derivative of qdot wrt q
+Mq = theta0(4);
+% Derivative of udot wrt delta
+Xd = theta0(5);
+% Derivative of qdot wrt delta
+Md = theta0(6);
+
+% Model parameters cell
+parameters = {'Xu', Xu ; 'Xq', Xq; 'Mu', Mu;...
+    'Mq', Mq; 'Xd', Xd; 'Md', Md};
+% Select continuous-time model
+fcn_type = 'c';
+
+% Assembly Frequency Response Data
+% data = frd(freqdata, faxis_masked);
+% Create grey model for identification
+greyobj = idgrey(model_fun, parameters, fcn_type);
+% Set grey identification options
+greyopt = greyestOptions('Display', 'on');
+
+% Identify model parameters with MATLAB Built-In function greyest()
+[fitmodel, ~] = greyest(data_to_fit, greyobj, greyopt);
+[est_params, est_unc] = getpvec(fitmodel);
+
+end
